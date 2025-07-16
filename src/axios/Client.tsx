48e2e5266c4c42006a7777bbe63a48fc0c42db94
irtazakhanx <irtazakhanx2003@gmail.com>
2025-07-16 17:29:22 +0500

    updated Backend url

--- src/axios/Client.tsx
+++ src/axios/Client.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: 'http://localhost:9000',
+  baseURL: 'https://task-app-backend-2pzo.onrender.com',
 });
 
 client.interceptors.request.use(
